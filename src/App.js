a4128db7dc6e4132f7fa99c816516394b7e642a2
KamogeloMafaesa <60877078+kamogeloo@users.noreply.github.com>
2021-12-02 16:52:19 +0200

    added files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import {Route, Switch} from 'react-router-dom';
+import Home from './pages/Home';
+import Footer from './components/Footer';
+import Blogs from './pages/blogPosts';
+import NetArt from './pages/netArt';
+
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-     
-        
-      </header>
+      <Navbar/>
+       <Switch>
+         <Route exact path='/' component={Home}/>
+         <Route export path='/blogPosts' component={Blogs}/>
+         <Route export path='/netArt' component={NetArt}/>
+       </Switch>
+       
+       <Footer/>
     </div>
+    
   );
 }
 
